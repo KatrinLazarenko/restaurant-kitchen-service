0a1d02bb6ecc0c71bff681ebded3bc2305825dc9
Katrin Lazarenko <katrin903@gmail.com>
2022-11-15 08:12:47 +0200

    Fixed secret key

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRETE_KEY']
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
@@ -76,7 +76,6 @@ TEMPLATES = [
     },
 ]
 
-# CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
